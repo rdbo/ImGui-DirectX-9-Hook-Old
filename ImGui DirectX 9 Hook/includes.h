32c5dbe6ac97047e13c7ed545752b4d797ddd916
Rdbo <57117082+rdbo@users.noreply.github.com>
2019-12-15 19:52:00 -0300

    Update includes.h

--- ImGui DirectX 9 Hook/includes.h
+++ ImGui DirectX 9 Hook/includes.h
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <Windows.h>
 #include <d3dx9.h>
-#include <detours.h>
+//#include <detours.h>
 #include "imgui/imgui.h"
 #include "imgui/imgui_impl_win32.h"
 #include "imgui/imgui_impl_dx9.h"
